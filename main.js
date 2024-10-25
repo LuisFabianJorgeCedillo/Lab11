ac86c7303ae046586d98630d5da136db558a9f0f
LuisFabianJorgeCedillo <fabianciittoo@gmail.com>
2024-10-25 11:46:23 -0400

    v3

--- main.js
+++ main.js
@@ -37,8 +37,7 @@ class User {
 let users = [
     new User(1, 'John Doe', 'johndoe', 'john@example.com', 'password123', new Date(), 'john.jpg', 'admin'),
     new User(2, 'Jane Smith', 'janesmith', 'jane@example.com', 'password123', new Date(), 'jane.jpg', 'user'),
-    new User(3, 'Robert Brown', 'robbrown', 'robert@example.com', 'password123', new Date(), 'robert.jpg', 'user'),
-    new User(4, 'Luis Fabian', 'Jorge', 'fabianciittoo@gamil.com','password123', new Date(), 'perfil.jpg', 'user')
+    new User(3, 'Luis Fabian', 'cedillo', 'fabianciittoo@gmail.com', 'password123', new Date(), 'fabian.jpg', 'user'),
 ];
 
 // CRUD Endpoints
